2ba3783d18f112373af50a1d34d48dec2fd99ed8
annomao <omaoannkerubo@gmail.com>
2022-04-06 13:56:57 +0300

    add solutions for the tests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(toDo = "go to the office"){
+  return `This Monday, I will ${toDo}.`;
+}
+
+function wrapAdjective(visualFlair = "*"){
+  return function (text = "special"){
+    return `You are ${visualFlair}${text}${visualFlair}!`;
+  }
+}
